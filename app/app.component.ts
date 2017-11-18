747607d58dd69e99342fd578eb91925778060997
The Ultimate Winner <harshadchawra@gmail.com>
2017-11-18 17:49:19 +0530

    second commit

---
+++ app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="/" class="navbar-brand">My Angular 2 App</a>
+                </div>
+            </nav>
+        </header>
+        <main>
+
+            <div class="row">
+                <div class="col-sm-4">
+
+                    <div *ngIf="users">
+                        <ul class="list-group users-list">
+                            <li class="list-group-item" *ngFor="let user of users">
+                                {{user.name}} ({{user.username}})
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="col-sm-8">
+                    <div class="jumbotron">
+                        <h1>Welcome to Our App!</h1>
+                        <p>{{message}}</p>
+                    </div>
+                </div>
+            </div>
+
+            
+
+            
+        </main>
+        <footer class="text-center">
+            Copyright &copy; 2016
+        </footer>
+    `,
+    styles: [`
+        .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0,2);}
+    `]
+})
+export class AppComponent {
+    message = "Hello";
+    users = [
+    {id:25, name : 'Harshad', username : 'Hchawra'},
+    {id:1, name : 'John', username:'Cenation'},
+    {id:17, name : 'Brock', username:'Lesnar'}
+];
+}
---
+++ dist/app/magic.js
@@ -0,0 +1,6 @@
+var Greeter = /** @class */ (function () {
+    function Greeter() {
+    }
+    return Greeter;
+}());
+//# sourceMappingURL=magic.js.map
